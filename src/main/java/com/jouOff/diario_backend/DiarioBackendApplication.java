2bd5581eba1809f1bd75a2902d0d449f4e4ee090
Johanna Monroy <johamonroy20@gmail.com>
2026-07-24 10:50:08 +0200

    chore: initialize Spring Boot project whit core dependencies

---
+++ src/main/java/com/jouOff/diario_backend/DiarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jouOff.diario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jouOff/diario_backend/DiarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jouOff.diario_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
